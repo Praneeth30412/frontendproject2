3628cf0b2c64d1c94e8181d87616c782d120cd32
Praneeth30412 <2400030412@kluniversity.in>
2025-07-29 15:39:02 +0530

    version1

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Page1 from './Page1.jsx';
 import Page2 from './Page2.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="frontendproject2">
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/page1" element={<Page1 />} />
